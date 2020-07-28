f950850901ab4248342645ccb18876f9361bee9e
cbk914 <cbk914@riseup.net>
2020-07-28 22:24:57 +0200

    Update

--- scanner.py
+++ scanner.py
@@ -12,7 +12,7 @@ print("\nScanning",target,"for ports 80,443...\n")
 for port in ports:
     portscan = scan_v.scan(target,str(port))
     print("\nPort ",port," is ",portscan['scan'][target]['tcp'][80]['state'])
-    print("\nPort ",port," is ",portscan['scan'][target]['tcp'][443]['state'])
+    #print("\nPort ",port," is ",portscan['scan'][target]['tcp'][443]['state'])
 
 print("\nHost ",target," is ",portscan['scan'][target]['status']['state'])
 
