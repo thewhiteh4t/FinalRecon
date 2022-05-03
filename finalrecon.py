341405760bf4ceb6cbbcde7124ccd88bd0f36ab7
thewhiteh4t <lohityapushkar@gmail.com>
2022-05-04 02:57:04 +0530

    v1.1.3 - update check and dependency check remvove and code cleanup

--- finalrecon.py
+++ finalrecon.py
@@ -3,12 +3,11 @@
 import os
 import sys
 import atexit
-import importlib.util
 
-R = '\033[31m' # red
-G = '\033[32m' # green
-C = '\033[36m' # cyan
-W = '\033[0m'  # white
+R = '\033[31m'  # red
+G = '\033[32m'  # green
+C = '\033[36m'  # cyan
+W = '\033[0m'   # white
 
 home = os.getenv('HOME')
 pid_path = home + '/.local/share/finalrecon/finalrecon.pid'
@@ -16,14 +15,15 @@ usr_data = home + '/.local/share/finalrecon/dumps/'
 conf_path = home + '/.config/finalrecon'
 path_to_script = os.path.dirname(os.path.realpath(__file__))
 src_conf_path = path_to_script + '/conf/'
+meta_file_path = path_to_script + '/metadata.json'
 fail = False
 
 if os.path.isfile(pid_path):
-	print(R + '[-]' + C + ' One instance of FinalRecon is already running!' + W)
+	print(f'{R}[-] {C}One instance of FinalRecon is already running!{W}')
 	with open(pid_path, 'r') as pidfile:
 		pid = pidfile.read()
-	print(G + '[+]' + C + ' PID : ' + W + str(pid))
-	print(G + '[>]' + C + ' If FinalRecon crashed, execute : ' + W + 'rm {}'.format(pid_path))
+	print(f'{G}[+] {C}PID :{W} {str(pid)}')
+	print(f'{G}[>] {C}If FinalRecon crashed, execute :{W} rm {pid_path}')
 	sys.exit(1)
 else:
 	os.makedirs(os.path.dirname(pid_path), exist_ok=True)
@@ -36,17 +36,14 @@ else:
 	import shutil
 	shutil.copytree(src_conf_path, conf_path, dirs_exist_ok=True)
 
-with open(path_to_script + '/requirements.txt', 'r') as rqr:
-	pkg_list = rqr.read().strip().split('\n')
-
 import argparse
 
-version = '1.1.2'
+version = '1.1.3'
 gh_version = ''
 twitter_url = ''
 discord_url = ''
 
-parser = argparse.ArgumentParser(description='FinalRecon - The Last Web Recon Tool You Will Need | v{}'.format(version))
+parser = argparse.ArgumentParser(description=f'FinalRecon - The Last Web Recon Tool You Will Need | v{version}')
 parser.add_argument('url', help='Target URL')
 parser.add_argument('--headers', help='Header Information', action='store_true')
 parser.add_argument('--sslinfo', help='SSL Certificate Information', action='store_true')
@@ -73,18 +70,18 @@ ext_help.add_argument('-p', type=int, help='Port for Traceroute [ Default : 80 /
 ext_help.add_argument('-tt', type=float, help='Traceroute Timeout [ Default : 1.0 ]')
 ext_help.add_argument('-o', help='Export Output [ Default : txt ] [ Available : xml, csv ]')
 ext_help.set_defaults(
-	t = 30,
-	T = 30.0,
-	w = path_to_script + '/wordlists/dirb_common.txt',
-	r = False,
-	s = True,
-	sp = 443,
-	d = '1.1.1.1',
-	e = '',
-	m = 'UDP',
-	p = 33434,
-	tt = 1.0,
-	o = 'txt')
+	t=30,
+	T=30.0,
+	w=path_to_script + '/wordlists/dirb_common.txt',
+	r=False,
+	s=True,
+	sp=443,
+	d='1.1.1.1',
+	e='',
+	m='UDP',
+	p=33434,
+	tt=1.0,
+	o='txt')
 
 try:
 	args = parser.parse_args()
@@ -116,44 +113,25 @@ port = args.p
 tr_tout = args.tt
 output = args.o
 
-import json
 import socket
 import requests
 import datetime
 import ipaddress
 import tldextract
+from json import loads
 
 type_ip = False
 data = {}
 meta = {}
 
-def fetch_meta():
-	global gh_version, twitter_url, discord_url
-	try:
-		rqst = requests.get('https://raw.githubusercontent.com/thewhiteh4t/finalrecon/master/metadata.json', timeout=5)
-		sc = rqst.status_code
-		if sc == 200:
-			metadata = rqst.text
-			json_data = json.loads(metadata)
-			gh_version = json_data['metadata']['version']
-			twitter_url = json_data['metadata']['twitter']
-			discord_url = json_data['metadata']['discord']
-		else:
-			with open('metadata.json', 'r') as metadata:
-				json_data = json.loads(metadata.read())
-				gh_version = json_data['metadata']['version']
-				twitter_url = json_data['metadata']['twitter']
-				discord_url = json_data['metadata']['discord']
-	except Exception as exc:
-		print('\n' + R + '[-]' + C + ' Exception : ' + W + str(exc))
-		with open('metadata.json', 'r') as metadata:
-			json_data = json.loads(metadata.read())
-			gh_version = json_data['metadata']['version']
-			twitter_url = json_data['metadata']['twitter']
-			discord_url = json_data['metadata']['discord']
 
 def banner():
-	banner = r'''
+	with open(meta_file_path, 'r') as metadata:
+		json_data = loads(metadata.read())
+		twitter_url = json_data['twitter']
+		comms_url = json_data['comms']
+
+	art = r'''
  ______  __   __   __   ______   __
 /\  ___\/\ \ /\ "-.\ \ /\  __ \ /\ \
 \ \  __\\ \ \\ \ \-.  \\ \  __ \\ \ \____
@@ -164,18 +142,12 @@ def banner():
 \ \  __< \ \  __\ \ \ \____\ \ \/\ \\ \ \-.  \
  \ \_\ \_\\ \_____\\ \_____\\ \_____\\ \_\\"\_\
   \/_/ /_/ \/_____/ \/_____/ \/_____/ \/_/ \/_/'''
-	print(G + banner + W + '\n')
-	print(G + '[>]' + C + ' Created By : ' + W + 'thewhiteh4t')
-	print(G + ' |---> ' + C + 'Twitter : ' + W + twitter_url)
-	print(G + ' |---> ' + C + 'Discord : ' + W + discord_url)
-	print(G + '[>]' + C + ' Version    : ' + W + version + '\n')
+	print(f'{G}{art}{W}\n')
+	print(f'{G}[>]{C} Created By   :{W} thewhiteh4t')
+	print(f'{G} |--->{C} Twitter   :{W} {twitter_url}')
+	print(f'{G} |--->{C} Community :{W} {comms_url}')
+	print(f'{G}[>]{C} Version      :{W} {version}\n')
 
-def ver_check():
-	print(G + '[+]' + C + ' Checking for Updates...', end='')
-	if version == gh_version:
-		print(C + '[' + G + ' Up-To-Date ' + C +']' + '\n')
-	else:
-		print(C + '[' + G + ' Available : {} '.format(gh_version) + C + ']' + '\n')
 
 def full_recon():
 	from modules.sslinfo import cert
@@ -191,7 +163,7 @@ def full_recon():
 	cert(hostname, sslp, output, data)
 	whois_lookup(ip, output, data)
 	dnsrec(domain, output, data)
-	if type_ip == False:
+	if type_ip is False:
 		subdomains(domain, tout, output, data, conf_path)
 	else:
 		pass
@@ -200,24 +172,23 @@ def full_recon():
 	crawler(target, output, data)
 	hammer(target, threads, tout, wdlist, redir, sslv, dserv, output, data, filext)
 
-try:
-	fetch_meta()
-	banner()
-	ver_check()
 
-	if target.startswith(('http', 'https')) == False:
-		print(R + '[-]' + C + ' Protocol Missing, Include ' + W + 'http://' + C + ' or ' + W + 'https://' + '\n')
+try:
+	banner()
+
+	if target.startswith(('http', 'https')) is False:
+		print(f'{R}[-] {C}Protocol Missing, Include {W}http:// {C}or{W} https:// \n')
 		os.remove(pid_path)
 		sys.exit(1)
 	else:
 		pass
 
-	if target.endswith('/') == True:
+	if target.endswith('/') is True:
 		target = target[:-1]
 	else:
 		pass
 
-	print (G + '[+]' + C + ' Target : ' + W + target)
+	print(f'{G}[+] {C}Target : {W}{target}')
 	ext = tldextract.extract(target)
 	domain = ext.registered_domain
 	hostname = '.'.join(part for part in ext if part)
@@ -226,12 +197,12 @@ try:
 		ipaddress.ip_address(hostname)
 		type_ip = True
 		ip = hostname
-	except:
+	except Exception:
 		try:
 			ip = socket.gethostbyname(hostname)
-			print ('\n' + G + '[+]' + C + ' IP Address : ' + W + str(ip))
+			print(f'\n{G}[+] {C}IP Address : {W}{str(ip)}')
 		except Exception as e:
-			print ('\n' + R + '[-]' + C + ' Unable to Get IP : ' + W + str(e))
+			print(f'\n{R}[-] {C}Unable to Get IP : {W}{str(e)}')
 			os.remove(pid_path)
 			sys.exit(1)
 
@@ -248,49 +219,49 @@ try:
 		fpath = usr_data
 		fname = fpath + hostname + '.' + output
 		if not os.path.exists(fpath):
-				os.makedirs(fpath)
+			os.makedirs(fpath)
 		output = {
 			'format': output,
 			'file': fname,
 			'export': False
-			}
+		}
 
 	from modules.export import export
 
-	if full == True:
+	if full is True:
 		full_recon()
 
-	if headinfo == True:
+	if headinfo is True:
 		from modules.headers import headers
 		headers(target, output, data)
 
-	if sslinfo == True:
+	if sslinfo is True:
 		from modules.sslinfo import cert
 		cert(hostname, sslp, output, data)
 
-	if whois == True:
+	if whois is True:
 		from modules.whois import whois_lookup
 		whois_lookup(ip, output, data)
 
-	if crawl == True:
+	if crawl is True:
 		from modules.crawler import crawler
 		crawler(target, output, data)
 
-	if dns == True:
+	if dns is True:
 		from modules.dns import dnsrec
 		dnsrec(domain, output, data)
 
-	if subd == True and type_ip == False:
+	if subd is True and type_ip is False:
 		from modules.subdom import subdomains
 		subdomains(domain, tout, output, data, conf_path)
-	elif subd == True and type_ip == True:
-		print(R + '[-]' + C + ' Sub-Domain Enumeration is Not Supported for IP Addresses' + W + '\n')
+	elif subd is True and type_ip is True:
+		print(f'{R}[-] {C}Sub-Domain Enumeration is Not Supported for IP Addresses{W}\n')
 		os.remove(pid_path)
 		sys.exit(1)
 	else:
 		pass
 
-	if trace == True:
+	if trace is True:
 		from modules.traceroute import troute
 		if mode == 'TCP' and port == 33434:
 			port = 80
@@ -298,22 +269,22 @@ try:
 		else:
 			troute(ip, mode, port, tr_tout, output, data)
 
-	if pscan == True:
+	if pscan is True:
 		from modules.portscan import ps
 		ps(ip, output, data)
 
-	if dirrec == True:
+	if dirrec is True:
 		from modules.dirrec import hammer
 		hammer(target, threads, tout, wdlist, redir, sslv, dserv, output, data, filext)
 
-	if any([full, headinfo, sslinfo, whois, crawl, dns, subd, trace, pscan, dirrec]) != True:
-		print ('\n' + R + '[-] Error : ' + C + 'At least One Argument is Required with URL' + W)
+	if any([full, headinfo, sslinfo, whois, crawl, dns, subd, trace, pscan, dirrec]) is not True:
+		print(f'\n{R}[-] Error : {C}At least One Argument is Required with URL{W}')
 		output = 'None'
 		os.remove(pid_path)
 		sys.exit(1)
 
 	end_time = datetime.datetime.now() - start_time
-	print ('\n' + G + '[+]' + C + ' Completed in ' + W + str(end_time) + '\n')
+	print(f'\n{G}[+] {C}Completed in {W}{str(end_time)}\n')
 
 	@atexit.register
 	def call_export():
@@ -326,6 +297,6 @@ try:
 	os.remove(pid_path)
 	sys.exit()
 except KeyboardInterrupt:
-	print (R + '[-]' + C + ' Keyboard Interrupt.' + W + '\n')
+	print(f'{R}[-] {C}Keyboard Interrupt.{W}\n')
 	os.remove(pid_path)
 	sys.exit(130)
--- modules/crawler.py
+++ modules/crawler.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import os
 import re
 import bs4
 import lxml
@@ -12,15 +11,15 @@ import tldextract
 from datetime import date
 requests.packages.urllib3.disable_warnings()
 
-R = '\033[31m' # red
-G = '\033[32m' # green
-C = '\033[36m' # cyan
-W = '\033[0m'  # white
-Y = '\033[33m' # yellow
+R = '\033[31m'  # red
+G = '\033[32m'  # green
+C = '\033[36m'  # cyan
+W = '\033[0m'   # white
+Y = '\033[33m'  # yellow
 
 user_agent = {
-	'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0'
-	}
+	'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0'
+}
 
 soup = ''
 r_url = ''
@@ -36,6 +35,7 @@ img_total = []
 js_crawl_total = []
 sm_crawl_total = []
 wayback_total = []
+
 
 def crawler(target, output, data):
 	global soup, r_url, sm_url
@@ -57,7 +57,7 @@ def crawler(target, output, data):
 		temp_tgt = target.split('://')[1]
 		pattern = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{2,5}'
 		custom = bool(re.match(pattern, temp_tgt))
-		if custom == True:
+		if custom is True:
 			r_url = protocol + '://' + temp_tgt + '/robots.txt'
 			sm_url = protocol + '://' + temp_tgt + '/sitemap.xml'
 		else:
@@ -84,26 +84,28 @@ def crawler(target, output, data):
 
 		out(target, output, data)
 	else:
-		print (R + '[-]' + C + ' Status : ' + W + str(sc))
+		print(R + '[-]' + C + ' Status : ' + W + str(sc))
+
 
 def url_filter(target):
 	global url
 
-	if all([url.startswith('/') == True, url.startswith('//') == False]):
+	if all([url.startswith('/') is True, url.startswith('//') is False]):
 		url = target + url
 	else:
 		pass
 
-	if all([url.find('http://') == -1,
+	if all([
+		url.find('http://') == -1,
 		url.find('https://') == -1]):
-
 		url = url.replace('//', 'http://')
 		url = url.replace('../', target + '/')
 		url = url.replace('./', target + '/')
 	else:
 		pass
 
-	if all([url.find('//') == -1,
+	if all([
+		url.find('//') == -1,
 		url.find('../') == -1,
 		url.find('./') == -1,
 		url.find('http://') == -1,
@@ -112,6 +114,7 @@ def url_filter(target):
 		url = target + '/' + url
 	else:
 		pass
+
 
 async def wayback(target):
 	global wayback_total
@@ -122,16 +125,12 @@ async def wayback(target):
 		domain = ext.domain
 	domain_query = domain + '/*'
 
-	#today = date.today().strftime("%Y%m%d")
-	#past = date.today() + relativedelta(months=-6)
-	#past = past.strftime("%Y%m%d")
-
 	curr_yr = date.today().year
 	last_yr = curr_yr - 1
 
-	print(Y + '[!]' + C + ' Checking Availability on Wayback Machine' + W, end = '')
+	print(Y + '[!]' + C + ' Checking Availability on Wayback Machine' + W, end='')
 	wm_avail = 'http://archive.org/wayback/available'
-	avail_data = { 'url': domain }
+	avail_data = {'url': domain}
 
 	try:
 		check_rqst = requests.get(wm_avail, params=avail_data, timeout=10)
@@ -150,14 +149,14 @@ async def wayback(target):
 	except Exception as e:
 		print('\n' + R + '[-] Exception : ' + C + str(e) + W)
 
-	if is_avail == True:
-		print(Y + '[!]' + C + ' Requesting Wayback Machine' + W, end = '')
+	if is_avail is True:
+		print(Y + '[!]' + C + ' Requesting Wayback Machine' + W, end='')
 		wm_url = 'http://web.archive.org/cdx/search/cdx'
 
 		data = {
-	    	'url': domain_query,
-	    	'fl': 'original',
-	    	'fastLatest': 'true',
+			'url': domain_query,
+			'fl': 'original',
+			'fastLatest': 'true',
 			'from': '{}'.format(str(last_yr)),
 			'to': '{}'.format(str(curr_yr)),
 			'filter': 'statuscode:200'
@@ -180,22 +179,24 @@ async def wayback(target):
 		except Exception as e:
 			print('\n' + R + '[-] Exception : ' + C + str(e) + W)
 
+
 async def robots(target):
 	global url, r_url, r_total
-	print(G + '[+]' + C + ' Looking for robots.txt' + W, end = '')
+	print(G + '[+]' + C + ' Looking for robots.txt' + W, end='')
 
 	try:
 		r_rqst = requests.get(r_url, headers=user_agent, verify=False, timeout=10)
 		r_sc = r_rqst.status_code
 		if r_sc == 200:
 			print(G + '['.rjust(9, '.') + ' Found ]' + W)
-			print(G + '[+]' + C + ' Extracting robots Links', end = '')
+			print(G + '[+]' + C + ' Extracting robots Links', end='')
 			r_page = r_rqst.text
 			r_scrape = r_page.split('\n')
 			for entry in r_scrape:
-				if (entry.find('Disallow') == 0 or
-					entry.find('Allow') == 0 or
-					entry.find('Sitemap') == 0):
+				if any([
+					entry.find('Disallow') == 0,
+					entry.find('Allow') == 0,
+					entry.find('Sitemap') == 0]):
 
 					url = entry.split(': ')
 					try:
@@ -203,9 +204,9 @@ async def robots(target):
 						url = url.strip()
 						url_filter(target)
 						r_total.append(url)
-						if url.endswith('xml') == True:
+						if url.endswith('xml') is True:
 							sm_total.append(url)
-					except:
+					except Exception:
 						pass
 
 			r_total = set(r_total)
@@ -219,21 +220,22 @@ async def robots(target):
 	except Exception as e:
 		print('\n' + R + '[-] Exception : ' + C + str(e) + W)
 
+
 async def sitemap():
 	global url, sm_url, total, sm_total
-	print(G + '[+]' + C + ' Looking for sitemap.xml' + W, end = '')
+	print(G + '[+]' + C + ' Looking for sitemap.xml' + W, end='')
 	try:
 		sm_rqst = requests.get(sm_url, headers=user_agent, verify=False, timeout=10)
 		sm_sc = sm_rqst.status_code
 		if sm_sc == 200:
 			print(G + '['.rjust(8, '.') + ' Found ]' + W)
-			print(G + '[+]' + C + ' Extracting sitemap Links', end = '')
+			print(G + '[+]' + C + ' Extracting sitemap Links', end='')
 			sm_page = sm_rqst.content
 			sm_soup = bs4.BeautifulSoup(sm_page, 'xml')
 			links = sm_soup.find_all('loc')
 			for url in links:
 				url = url.get_text()
-				if url != None:
+				if url is not None:
 					sm_total.append(url)
 
 			sm_total = set(sm_total)
@@ -246,37 +248,40 @@ async def sitemap():
 	except Exception as e:
 		print('\n' + R + '[-] Exception : ' + C + str(e))
 
+
 async def css(target):
 	global url, soup, total, css_total
-	print(G + '[+]' + C + ' Extracting CSS Links' + W, end = '')
+	print(G + '[+]' + C + ' Extracting CSS Links' + W, end='')
 	css = soup.find_all('link')
 
 	for link in css:
 		url = link.get('href')
-		if url != None and '.css' in url:
+		if url is not None and '.css' in url:
 			url_filter(target)
 			css_total.append(url)
 
 	css_total = set(css_total)
 	print(G + '['.rjust(11, '.') + ' {} ]'.format(str(len(css_total))) + W)
 
+
 async def js(target):
 	global url, total, js_total
-	print(G + '[+]' + C + ' Extracting Javascript Links' + W, end = '')
+	print(G + '[+]' + C + ' Extracting Javascript Links' + W, end='')
 	js = soup.find_all('script')
 
 	for link in js:
 		url = link.get('src')
-		if url != None and '.js' in url:
+		if url is not None and '.js' in url:
 			url_filter(target)
 			js_total.append(url)
 
 	js_total = set(js_total)
 	print(G + '['.rjust(4, '.') + ' {} ]'.format(str(len(js_total))))
 
+
 async def internal_links(target):
 	global total, int_total
-	print(G + '[+]' + C + ' Extracting Internal Links' + W, end = '')
+	print(G + '[+]' + C + ' Extracting Internal Links' + W, end='')
 
 	ext = tldextract.extract(target)
 	domain = ext.registered_domain
@@ -284,16 +289,17 @@ async def internal_links(target):
 	links = soup.find_all('a')
 	for link in links:
 		url = link.get('href')
-		if url != None:
+		if url is not None:
 			if domain in url:
 				int_total.append(url)
 
 	int_total = set(int_total)
 	print(G + '['.rjust(6, '.') + ' {} ]'.format(str(len(int_total))))
 
+
 async def external_links(target):
 	global total, ext_total
-	print(G + '[+]' + C + ' Extracting External Links' + W, end = '')
+	print(G + '[+]' + C + ' Extracting External Links' + W, end='')
 
 	ext = tldextract.extract(target)
 	domain = ext.registered_domain
@@ -301,30 +307,32 @@ async def external_links(target):
 	links = soup.find_all('a')
 	for link in links:
 		url = link.get('href')
-		if url != None:
+		if url is not None:
 			if domain not in url and 'http' in url:
 				ext_total.append(url)
 
 	ext_total = set(ext_total)
 	print(G + '['.rjust(6, '.') + ' {} ]'.format(str(len(ext_total))))
 
+
 async def images(target):
 	global url, total, img_total
-	print(G + '[+]' + C + ' Extracting Images' + W, end = '')
+	print(G + '[+]' + C + ' Extracting Images' + W, end='')
 	images = soup.find_all('img')
 
 	for link in images:
 		url = link.get('src')
-		if url != None and len(url) > 1:
+		if url is not None and len(url) > 1:
 			url_filter(target)
 			img_total.append(url)
 
 	img_total = set(img_total)
 	print(G + '['.rjust(14, '.') + ' {} ]'.format(str(len(img_total))))
 
+
 async def sm_crawl():
 	global sm_crawl_total
-	print(G + '[+]' + C + ' Crawling Sitemaps' + W, end = '')
+	print(G + '[+]' + C + ' Crawling Sitemaps' + W, end='')
 
 	threads = []
 	
@@ -338,7 +346,7 @@ async def sm_crawl():
 				links = sm_soup.find_all('loc')
 				for url in links:
 					url = url.get_text()
-					if url != None:
+					if url is not None:
 						sm_crawl_total.append(url)
 			elif sm_sc == 404:
 				print(R + '['.rjust(8, '.') + ' Not Found ]' + W)
@@ -349,7 +357,7 @@ async def sm_crawl():
 
 	for site_url in sm_total:
 		if site_url != sm_url:
-			if site_url.endswith('xml') == True:
+			if site_url.endswith('xml') is True:
 				t = threading.Thread(target=fetch, args=[site_url])
 				t.daemon = True
 				threads.append(t)
@@ -361,9 +369,10 @@ async def sm_crawl():
 	sm_crawl_total = set(sm_crawl_total)
 	print(G + '['.rjust(14, '.') + ' {} ]'.format(str(len(sm_crawl_total))))
 
+
 async def js_crawl():
 	global js_crawl_total
-	print(G + '[+]' + C + ' Crawling Javascripts' + W, end = '')
+	print(G + '[+]' + C + ' Crawling Javascripts' + W, end='')
 
 	threads = []
 
@@ -395,6 +404,7 @@ async def js_crawl():
 	js_crawl_total = set(js_crawl_total)
 	print(G + '['.rjust(11, '.') + ' {} ]'.format(str(len(js_crawl_total))))
 
+
 def out(target, output, data):
 	global total
 
@@ -423,45 +433,45 @@ def out(target, output, data):
 
 			data['module-Crawler'].update(
 				{
-					'Count ( Robots )':      str(len(r_total)),
-					'Count ( Sitemap )':     str(len(sm_total)),
-					'Count ( CSS )':         str(len(css_total)),
-					'Count ( JS )':          str(len(js_total)),
-					'Count ( Links in JS )':       str(len(js_crawl_total)),
+					'Count ( Robots )': str(len(r_total)),
+					'Count ( Sitemap )': str(len(sm_total)),
+					'Count ( CSS )': str(len(css_total)),
+					'Count ( JS )': str(len(js_total)),
+					'Count ( Links in JS )': str(len(js_crawl_total)),
 					'Count ( Links in Sitemaps )': str(len(sm_crawl_total)),
-					'Count ( Internal )':    str(len(int_total)),
-					'Count ( External )':    str(len(ext_total)),
-					'Count ( Images )':      str(len(img_total)),
+					'Count ( Internal )': str(len(int_total)),
+					'Count ( External )': str(len(ext_total)),
+					'Count ( Images )': str(len(img_total)),
 					'count ( Wayback Machine )': str(len(wayback_total)),
 					'Count ( Total )': str(len(total))
 				})
-			
+
 			if len(r_total) != 0:
 				data['module-Crawler'].update({'Robots': list(r_total)})
-			
+
 			if len(sm_total) != 0:
 				data['module-Crawler'].update({'Sitemaps': list(sm_total)})
-			
+
 			if len(css_total) != 0:
 				data['module-Crawler'].update({'CSS': list(css_total)})
-			
+
 			if len(js_total) != 0:
 				data['module-Crawler'].update({'Javascripts': list(js_total)})
 
 			if len(js_crawl_total) != 0:
 				data['module-Crawler'].update({'Links inside Javascripts': list(js_crawl_total)})
-			
+
 			if len(sm_crawl_total) != 0:
 				data['module-Crawler'].update({'Links Inside Sitemaps': list(sm_crawl_total)})
-			
+
 			if len(int_total) != 0:
 				data['module-Crawler'].update({'Internal Links': list(int_total)})
-		
+
 			if len(ext_total) != 0:
 				data['module-Crawler'].update({'External Links': list(ext_total)})
-			
+
 			if len(img_total) != 0:
 				data['module-Crawler'].update({'Images': list(img_total)})
-			
+
 			if len(wayback_total) != 0:
 				data['module-Crawler'].update({'Wayback Machine': list(wayback_total)})
